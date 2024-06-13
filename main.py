798286b2517bb5ea41bb0389576fff7094c1a4dd
Omar1202 <69159557+Omar1202@users.noreply.github.com>
2024-06-13 12:52:32 -0500

    groq transformation

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
-from langchain_openai import OpenAI
+from langchain_groq import ChatGroq
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 
 def generate_response(txt):
-    llm = OpenAI(
+    llm = ChatGroq(
+        model="llama3-70b-8192",
         temperature=0,
-        openai_api_key=openai_api_key
+        api_key=groq_api_key
     )
     text_splitter = CharacterTextSplitter()
     texts = text_splitter.split_text(txt)
@@ -16,7 +17,7 @@ def generate_response(txt):
         llm,
         chain_type="map_reduce"
     )
-    return chain.run(docs)
+    return chain.invoke(docs)
 
 st.set_page_config(
     page_title = "Writing Text Summarization"
@@ -31,16 +32,16 @@ txt_input = st.text_area(
 
 result = []
 with st.form("summarize_form", clear_on_submit=True):
-    openai_api_key = st.text_input(
-        "OpenAI API Key",
+    groq_api_key = st.text_input(
+        "Groq API Key",
         type="password",
         disabled=not txt_input
     )
     submitted = st.form_submit_button("Submit")
-    if submitted and openai_api_key.startswith("sk-"):
+    if submitted and groq_api_key.startswith("gsk_"):
         response = generate_response(txt_input)
         result.append(response)
-        del openai_api_key
+        del groq_api_key
 
 if len(result):
-    st.info(response)
+    st.info(response["output_text"])
